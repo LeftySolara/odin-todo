e44b537d8721175313e2da1abe588179047af3d4
Jalen Adams <jalen@jalenkadams.me>
2021-04-13 17:02:22 -0400

    Add: support for subtasks

--- src/factories/__test__/task.test.js
+++ src/factories/__test__/task.test.js
@@ -25,4 +25,42 @@ describe('task factory', () => {
       expect(task).toBeNull();
     });
   });
+
+  describe('when editing a task', () => {
+    let exampleTask;
+
+    beforeEach(() => {
+      exampleTask = createTask(0, 'Example task', 'Needs to be done');
+    });
+
+    test('should allow the creation of subtasks', () => {
+      expect(exampleTask.subtaskCount()).toBe(0);
+      exampleTask.addSubtask('This is a subtask');
+      expect(exampleTask.subtaskCount()).toBe(1);
+    });
+
+    describe('when deleting subtasks', () => {
+      test('should do nothing if no subtasks exist', () => {
+        exampleTask.deleteSubtask(0);
+        expect(exampleTask.subtaskCount()).toBe(0);
+      });
+
+      test('should allow the deletion of subtasks', () => {
+        exampleTask.addSubtask('Subtask1');
+        exampleTask.addSubtask('Subtask2');
+        exampleTask.addSubtask('Subtask3');
+        exampleTask.deleteSubtask(1);
+
+        expect(exampleTask.subtaskCount()).toBe(2);
+      });
+
+      test('should do nothing if requested task is not in list', () => {
+        exampleTask.addSubtask('Subtask1');
+        exampleTask.addSubtask('Subtask2');
+        exampleTask.deleteSubtask(5);
+
+        expect(exampleTask.subtaskCount()).toBe(2);
+      });
+    });
+  });
 });
--- src/factories/task.js
+++ src/factories/task.js
@@ -4,6 +4,17 @@ const PRIORITIES = {
   HIGH: 2,
 };
 
+const STATUSES = {
+  NOT_STARTED: 0,
+  IN_PROGRESS: 1,
+  COMPLETE: 2,
+};
+
+const createSubtask = (id, description) => {
+  const status = STATUSES.NOT_STARTED;
+  return { id, description, status };
+};
+
 /**
  * Creates a new Task object.
  *
@@ -23,7 +34,33 @@ const createTask = (
     return null;
   }
 
-  return { id, title, description, dueDate, priority };
+  let subtasks = [];
+  let nextSubtaskID = 0;
+
+  const addSubtask = (taskDescription) => {
+    const subtask = createSubtask(nextSubtaskID, taskDescription);
+    subtasks.push(subtask);
+    nextSubtaskID += 1;
+  };
+
+  const deleteSubtask = (subtaskID) => {
+    subtasks = subtasks.filter((subtask) => {
+      return subtask.id !== subtaskID;
+    });
+  };
+
+  const subtaskCount = () => subtasks.length;
+
+  return {
+    id,
+    title,
+    description,
+    dueDate,
+    priority,
+    addSubtask,
+    deleteSubtask,
+    subtaskCount,
+  };
 };
 
 export default createTask;
