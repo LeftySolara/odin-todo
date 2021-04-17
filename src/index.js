8f55a4bdbe8446d867942cc64b23fe0226485f6d
Jalen Adams <jalen@jalenkadams.me>
2021-04-17 12:03:35 -0400

    Make: set up Firebase connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/app';
+import Firebase, { FirebaseContext } from './components/firebase';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
