a13daec8f04b85419ebbda60f1d0ede54c181732
Jalen Adams <jalen@jalenkadams.me>
2021-04-18 11:00:27 -0400

    Add: user session handling

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../navigation';
@@ -12,6 +12,7 @@ import AccountPage from '../account';
 import AdminPage from '../admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../firebase';
 
 const LINKS = [
   { label: 'Sign In', to: ROUTES.SIGN_IN },
@@ -21,22 +22,35 @@ const LINKS = [
   { label: 'Admin', to: ROUTES.ADMIN },
 ];
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation links={LINKS} />
-      <hr />
+const App = (props) => {
+  const { firebase } = props;
+  const [authUser, setAuthUser] = useState(null);
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPasswordPage} />
-      <Route path={ROUTES.CHANGE_PASSWORD} component={ChangePasswordPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
-);
+  useEffect(() => {
+    firebase.auth.onAuthStateChanged((user) => {
+      // eslint-disable-next-line no-unused-expressions
+      user ? setAuthUser(user) : setAuthUser(null);
+    });
+    return null;
+  }, [authUser]);
 
-export default App;
+  return (
+    <Router>
+      <div>
+        <Navigation links={LINKS} authUser={authUser} />
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPasswordPage} />
+        <Route path={ROUTES.CHANGE_PASSWORD} component={ChangePasswordPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route path={ROUTES.ADMIN} component={AdminPage} />
+      </div>
+    </Router>
+  );
+};
+
+export default withFirebase(App);
--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -4,23 +4,44 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../signOut';
 import styles from './navigation.module.css';
 
+import * as ROUTES from '../../constants/routes';
+
 const Navigation = (props) => {
-  const { links } = props;
+  const { authUser } = props;
 
   return (
     <div className={styles.navigation}>
-      <ul className={styles['navigation-list']}>
-        {links.map((link) => (
-          <li className={styles['navigation-list-item']}>
-            <Link to={link.to}>{link.label}</Link>
-          </li>
-        ))}
-        <li>
-          <SignOutButton />
-        </li>
-      </ul>
+      {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
     </div>
   );
 };
+
+const NavigationAuth = () => (
+  <ul className={styles['navigation-list']}>
+    <li className={styles['navigation-list-item']}>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li className={styles['navigation-list-item']}>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li className={styles['navigation-list-item']}>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul className={styles['navigation-list']}>
+    <li className={styles['navigation-list-item']}>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li className={styles['navigation-list-item']}>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
+);
 
 export default Navigation;
