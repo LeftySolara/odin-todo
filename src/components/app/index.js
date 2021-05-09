ed03ae2a453e81e84511d3ea66c9687fa9106857
Jalen Adams <jalen@jalenkadams.me>
2021-05-08 21:33:56 -0400

    Add: sidebar to all project pages

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -10,6 +10,7 @@ import ChangePasswordPage from '../changePassword';
 import HomePage from '../home';
 import AccountPage from '../account';
 import AdminPage from '../admin';
+import ProjectPage from '../project';
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../session';
@@ -28,6 +29,7 @@ const App = () => (
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
+      <Route path={ROUTES.PROJECT} component={ProjectPage} />
     </div>
   </Router>
 );
---
+++ src/components/project/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectSidebar from '../sidebar';
+
+const ProjectPage = () => {
+  return (
+    <>
+      <ProjectSidebar />
+    </>
+  );
+};
+
+export default ProjectPage;
